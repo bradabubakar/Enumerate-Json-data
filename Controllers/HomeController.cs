1e4f4936296ae77d45eba20549748578fd923655
bradago <33542981+bradabubakar@users.noreply.github.com>
2020-06-16 02:38:55 +0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using opg_201910_interview.Models;
+using Newtonsoft.Json;
+
+
+namespace opg_201910_interview.Controllers
+{
+    public class HomeController : Controller
+    {
+       // private readonly ILogger<HomeController> _logger;
+        private readonly Appsettings _appSettings;
+        public HomeController(IConfiguration configuration)
+        {
+            _appSettings = new Appsettings();
+            configuration.GetSection("AppSettings").Bind(_appSettings);
+        }
+
+        public ContentResult Index()
+        {
+            try
+            {
+                var model = _appSettings.ClientSettings;
+
+                string json = JsonConvert.SerializeObject(model, Formatting.Indented);
+
+                return Content(json);
+            }
+            catch (Exception ex)
+            {
+                return Content("Error found!", ex.Message);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace opg_201910_interview.Models
+{
+
+    public class Appsettings
+    {
+        public Clientsetting[] ClientSettings { get; set; }
+    }
+
+    public class Clientsetting
+    {
+        public string ClientId { get; set; }
+        public string FileDirectoryPath { get; set; }
+        public string[] FileList { get; set; }
+    }
+
+    public class Logging
+    {
+        public Loglevel LogLevel { get; set; }
+    }
+
+    public class Loglevel
+    {
+        public string Default { get; set; }
+        public string Microsoft { get; set; }
+        public string MicrosoftHostingLifetime { get; set; }
+    }
+
+}
